5bf7bb33870786f45c5ffcd3dbde4797cc5b74fc
Ozan0062 <ozan0062@edu.zealand.dk>
2023-12-14 10:29:35 +0100

    Update Cors

--- VendingMachineREST/Program.cs
+++ VendingMachineREST/Program.cs
@@ -3,13 +3,6 @@ using VendingMachineREST.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "AllowAll", policy =>
@@ -19,6 +12,14 @@ builder.Services.AddCors(options =>
         AllowAnyHeader();
     });
 });
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 
 var userOptionBuilder = new DbContextOptionsBuilder<UsersDbContext>();
 userOptionBuilder.UseSqlServer("Data Source = mssql4.unoeuro.com; Initial Catalog =  ozanhs_dk_db_vending_machine; Persist Security Info = True; User ID = ozanhs_dk; Password = GcB6m5g4awRnbE29tyzp; TrustServerCertificate = True");
@@ -34,11 +35,11 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors("AllowAll");
+
 app.UseSwagger();
 
 app.UseSwaggerUI();
-
-app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
